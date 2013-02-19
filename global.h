42c9c08f8d0adca8287a6e9a684d4bfebf64e031
Fırat Salgür <firatsalgur@gmail.com>
2013-02-19 13:00:18 +0200

    add logging

---
+++ global.h
@@ -0,0 +1,10 @@
+#ifndef __GLOBAL_H
+#define __GLOBAL_H
+
+#include "log.h"
+
+#if defined ( WIN32 )
+  #define __func__ __FUNCTION__
+#endif
+
+#endif
---
+++ log.h
@@ -0,0 +1,8 @@
+#ifndef __LOG_H
+#define __LOG_H
+
+#include <glib.h>
+
+extern void log_init(GLogLevelFlags level);
+
+#endif
