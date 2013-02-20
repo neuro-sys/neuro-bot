068d06e8485298300102e99bbb0d4cf5a49a08c4
Üstün Ergenoglu <ustun.ergenoglu@boogiesoftware.com>
2013-02-20 09:57:24 +0200

    Fix OS X crashes.

--- socket.h
+++ socket.h
@@ -1,13 +1,13 @@
 #ifndef __SOCKET_H
 #define __SOCKET_H
 
-
+#include "global.h"
 
 #if defined ( WIN32 )
   #include <ws2tcpip.h>
   #define t_connect t_connect_win
   extern int t_connect_win(char *host, char *port);
-#elif defined (linux) || defined(__APPLE__)
+#elif defined (unix)
   #define t_connect t_connect_unix
   extern int t_connect_unix(char *host, char *port);
 #endif
