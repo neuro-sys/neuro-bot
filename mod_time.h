f418aba74c5ddece429825e1be267aeb481744fb
neuro <firatsalgur@gmail.com>
2013-02-20 17:35:21 +0400

    remove old files

--- mod_time.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MOD_TIME_H
-#define __MOD_TIME_H
-
-#include "irc.h"
-
-extern void mod_cmd_time(struct irc_t * irc);
-
-#endif
--- mod_title.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MOD_TITLE_H
-#define __MOD_TITLE_H
-
-#include "irc.h"
-
-extern void mod_line_title(struct irc_t * irc);
-
-#endif
-
