037fa6c2215051a0c864848c41647c6aca01dbed
Fırat Salgür <firatsalgur@gmail.com>
2013-02-21 22:38:37 +0200

    Update py_wrap.h

--- py_wrap.h
+++ py_wrap.h
@@ -1,7 +1,7 @@
 #ifndef __PY_WRAP_H
 #define __PY_WRAP_H
 
-GHashTable * mod_hash_map;
+extern GHashTable * mod_hash_map;
 
 extern int    py_load_modules();
 extern char * py_call_module(char * name);
