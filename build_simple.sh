b5149fc51ca608aea66ac6b7903e0cac075c0b97
U-NEUROSYS\neuro <neuro@neurosys.(none)>
2013-02-25 00:13:10 +0200

    update build script for -O0

--- build_simple.sh
+++ build_simple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc *.c modules/*.c `pkg-config.exe jansson libcurl glib-2.0 gio-2.0 --libs --cflags` `python2.7-config --libs --cflags` -g -I. -oircclient
+gcc -O0 *.c modules/*.c `pkg-config.exe jansson libcurl glib-2.0 gio-2.0 --libs --cflags` `python2.7-config --libs --cflags` -g -I. -oircclient -O0
 
 
