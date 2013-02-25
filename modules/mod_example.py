ef2b27c87640016f78492dc06232ab8d895b3d99
U-FIRAT\JFDEV <JFDEV@firat.(none)>
2013-02-25 16:08:18 +0200

    add python static var example

--- modules/mod_example.py
+++ modules/mod_example.py
@@ -1,8 +1,13 @@
+
 def mod_example(_from, _line):
-  return _from + " " + _line
+  mod_example.counter += 1
+  return "%d" % mod_example.counter
+mod_example.counter = 0
 
 def main():
   mod_test("#python", ".foo 42")
 
 if __name__ == '__main__':
    main()
+
+
