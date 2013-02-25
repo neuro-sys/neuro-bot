f3da375279c3bca835a71c01ebc198953cc2f0ea
Fırat Salgür <firatsalgur@gmail.com>
2013-02-25 22:11:47 +0200

    Update modules/mod_example.py

--- modules/mod_example.py
+++ modules/mod_example.py
@@ -1,11 +1,10 @@
-
 def mod_example(_from, _line):
   mod_example.counter += 1
   return "%d" % mod_example.counter
 mod_example.counter = 0
 
 def main():
-  mod_test("#python", ".foo 42")
+  mod_example("#python", ".foo 42")
 
 if __name__ == '__main__':
    main()
