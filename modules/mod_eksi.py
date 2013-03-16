72b8578004514f17bde03b218f260142309b0afa
Fırat Salgür <firatsalgur@gmail.com>
2013-03-16 04:29:00 +0200

    Update mod_eksi.py

--- modules/mod_eksi.py
+++ modules/mod_eksi.py
@@ -4,17 +4,8 @@ def mod_eksi(_from, _line):
     e = "+".join(_line.split()[1:])
     o = urllib2.build_opener()
     o.addheaders.append(('Cookie', 'zero-intro-skipped=1'))
-    print(e)
     f = o.open("http://www.eksisozluk.com/?q=" + e)
     m = re.search('content">(.*?)<', f.read())
     return nltk.clean_html(m.group(1))
-
-
-def main():
-    print(mod_eksi("", ".eksi iki kelime"))
-    return
-
-if __name__ == "__main__":
-    main()
-
+    
 
