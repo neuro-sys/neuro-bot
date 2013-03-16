076e09fc03a61219b030276b2b47755f1a832b63
Fırat Salgür <firatsalgur@gmail.com>
2013-03-16 04:03:24 +0200

    Update mod_eksi.py

--- modules/mod_eksi.py
+++ modules/mod_eksi.py
@@ -1,10 +1,10 @@
 import re, urllib2, nltk
 
 def mod_eksi(_from, _line):
-    entry = _line.split()[1]
-    opener = urllib2.build_opener()
-    opener.addheaders.append(('Cookie', 'zero-intro-skipped=1'))
-    f = opener.open("http://www.eksisozluk.com/?q=" + entry)
+    e = _line.split()[1]
+    o = urllib2.build_opener()
+    o.addheaders.append(('Cookie', 'zero-intro-skipped=1'))
+    f = o.open("http://www.eksisozluk.com/?q=" + e)
     m = re.search('content">(.*?)<', f.read())
     return nltk.clean_html(m.group(1))
 
