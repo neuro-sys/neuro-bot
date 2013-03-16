013c191795246c49fc8434636969b65fd0391751
Firat Salgur <firatsalgur@gmail.com>
2013-03-16 13:36:47 +0200

    prettify build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#run pkg-config --list-all to check libs
+[ "$1" == "rm" ] && rm *.o && exit
+
 LIBS="jansson libcurl glib-2.0 gio-2.0 python-2.7"
 CFLAGS="`pkg-config $LIBS --cflags` -I. -g"
 LDFLAGS="`pkg-config $LIBS --libs` -g"
