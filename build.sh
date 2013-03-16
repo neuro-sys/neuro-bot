9fbdd858675dd6447407bed5edd049a5a6a2f3c6
Firat Salgur <firatsalgur@gmail.com>
2013-03-16 05:53:56 +0200

    add -g to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#run pkg-config --list-all to check libs
 LIBS="jansson libcurl glib-2.0 gio-2.0 python-2.7"
-CFLAGS="`pkg-config $LIBS --cflags` -I."
-LDFLAGS="`pkg-config $LIBS --libs`"
+CFLAGS="`pkg-config $LIBS --cflags` -I. -g"
+LDFLAGS="`pkg-config $LIBS --libs` -g"
 
 gcc -c *.c modules/*.c $CFLAGS
 gcc *.o -o ircclient $LDFLAGS
