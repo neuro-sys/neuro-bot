1ab56c788063d7623768794b3e321d4279f5003b
Firat Salgur <firatsalgur@gmail.com>
2013-03-16 06:13:35 +0200

    fix to eksi mod

--- modules/mod_eksi.py
+++ modules/mod_eksi.py
@@ -1,11 +1,20 @@
 import re, urllib2, nltk
 
 def mod_eksi(_from, _line):
-    e = _line.split()[1]
+    e = "+".join(_line.split()[1:])
     o = urllib2.build_opener()
     o.addheaders.append(('Cookie', 'zero-intro-skipped=1'))
+    print(e)
     f = o.open("http://www.eksisozluk.com/?q=" + e)
     m = re.search('content">(.*?)<', f.read())
     return nltk.clean_html(m.group(1))
+
+
+def main():
+    print(mod_eksi("", ".eksi iki kelime"))
+    return
+
+if __name__ == "__main__":
+    main()
 
 
