f0dd3900c1e2b14194451b0095c48af9afc6fc64
U-FIRAT\JFDEV <JFDEV@firat.(none)>
2013-03-20 10:00:58 +0200

    fix potential buffer overruns

--- irc.h
+++ irc.h
@@ -4,19 +4,20 @@
 #include "session.h"
 
 #define MAX_MSG 510
+#define MAX_LEN 100
 
 struct srv_message_t {
-    char prefix[100];
-    char command[20];
-    char params[100];
+    char prefix[MAX_LEN];
+    char command[MAX_LEN];
+    char params[MAX_LEN];
 };
 
 struct irc_t {
     struct srv_message_t srv_msg;
     char                 request[MAX_MSG];
     char                 response[MAX_MSG];
-    char                 nick_to_msg[30];
-    char                 from[30];
+    char                 nick_to_msg[MAX_LEN];
+    char                 from[MAX_LEN];
     struct session_t     * session;
     char                 * admin;
 };
