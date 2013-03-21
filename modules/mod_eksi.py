d7fe588ac81ed6bb3161c860346eb3f5c5e982ab
Firat Salgur <firatsalgur@gmail.com>
2013-03-21 23:43:32 +0200

    upgrade/add eksi bot, provided by decaf

--- modules/mod_eksi.py
+++ modules/mod_eksi.py
@@ -1,14 +1,49 @@
-import re, urllib2, nltk, sys
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+# provided by decaf
+
+from urlgrabber import urlread
+from bs4 import BeautifulSoup
+
+import sys
+
+# sonuç boyutu [unicode char]
+LIMIT = 400
 
 def mod_eksi(_from, _line):
-    e = "+".join(_line.split()[1:])
-    o = urllib2.build_opener()
-    o.addheaders.append(('Cookie', 'zero-intro-skipped=1'))
-    try: f = o.open("http://www.eksisozluk.com/?q=" + e) 
-    except: return "Yok boyle bisi"
-    m = re.search('content">(.*?)<', f.read().replace("\\n", ""))
-    return nltk.clean_html(m.group(1))
-    
+    return daModule(_line.replace('.eksi ',''))
+
+def daModule(line):
+    url = buildUrl(line)
+    try:
+        st = urlread(url)
+    except:
+        return '[' +line+ '] yok böyle bi şii'
+    return parsit(st) 
+
+def buildUrl(line):
+    line = " ".join(line.split(' '))
+    return "http://eksisozluk.com/" + line
+
+def parsit(st):
+    def entry_isle(entry):
+        for br in entry.find_all('br'):
+            br.insert_before(' ')
+        return entry.text.replace('*','')
+
+    soup = BeautifulSoup(st)
+    tepelink = soup.find(id='title').a
+    baslik = "["+tepelink.text+"]"
+    href = "http://eksisozluk.com"+tepelink.get('href')
+    entries = soup.find_all("div", attrs={"class": "content"})
+    dt = [entry_isle(entry) for entry in entries]
+    netice = " | ".join(dt)
+    if len(netice) > (LIMIT - len(href) - len(baslik) + 4):
+        netice = netice[:LIMIT].rpartition("|")[0]
+    return (baslik + ' ' + netice + ' | ' + href).encode('utf-8')
+
 if __name__ == "__main__":
     print(mod_eksi("", sys.argv[1]))
+
 
