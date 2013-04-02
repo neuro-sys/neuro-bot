9bd79aa2008ab7ed1ceeb672aee4941a33138ad3
U-FIRAT\JFDEV <JFDEV@firat.(none)>
2013-04-02 13:37:48 +0300

    for the moment, use one char reading at a time using recv, which is inefficient.

--- network.c
+++ network.c
@@ -14,22 +14,6 @@
 #endif
 #include <sys/types.h>
 
-/*
- * via ii@suckless project.
- */
-static int read_line(int fd, size_t res_len, char *buf) {
-    size_t i = 0;
-    char c = 0;
-    do {
-        if(read(fd, &c, sizeof(char)) != sizeof(char))
-            return -1;
-        buf[i++] = c;
-    }
-    while(c != '\n' && i < res_len);
-    buf[i - 1] = 0;         /* eliminates '\n' */
-    return 0;
-}
-
 void network_connect(struct network_t * network)
 {
     char port_str[10];
@@ -42,20 +26,21 @@ void network_connect(struct network_t * network)
 
 int network_read_line(struct network_t * network, char * buf)
 {
-    struct timeval tv;
-    fd_set readfs;
+    int ret;
+    char ch;
+    while(1)
+    {
+      ret = recv(network->sockfd, &ch, 1, 0);
+      if (ret > 0)
+      {
+        *buf++ = ch;
+        if (ch == '\n')
+          break;
+      }
+    }
+    *buf = '\0';
 
-    tv.tv_sec = 120;
-    tv.tv_usec = 0;
-
-    FD_ZERO(&readfs);
-    FD_SET(network->sockfd, &readfs);
-
-    select(network->sockfd+1, &readfs, NULL, NULL, &tv);
-    if (FD_ISSET(network->sockfd, &readfs))
-        return read_line(network->sockfd, 1024, buf);
-
-    return -1;
+    return 1;
 }
 
 void network_send_message(struct network_t * network, char * message)
