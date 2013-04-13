2ae35d11f161dd5f96e104f057aa18bd14005298
Firat Salgur <firatsalgur@gmail.com>
2013-04-14 01:12:48 +0300

    .

--- modules/mod_youtube.c
+++ modules/mod_youtube.c
@@ -72,10 +72,7 @@ char * mod_youtube(struct irc_t * irc, char * reply_msg)
 
     if (youtube->valid)
     {
-        reply_msg[0] = 0x3;
-        reply_msg[1] = 4;
-
-        sprintf(reply_msg+2, "[%s] - [rating: %.2f/5, viewed: %s]", 
+        sprintf(reply_msg, "[%s] - [rating: %.2f/5, viewed: %s]", 
                 youtube->title, youtube->rating, youtube->view_count);
     }
     free(youtube);
