274dc13aa179b2b57970ea50aa97156ca3532efa
Üstün Ergenoglu <ego@ustun.fi>
2015-03-10 00:58:38 +0200

    Realistic oktay emulation
    In addition to the only LOL mode, oktay plugin now supports various
    oktay greetings and the famous channel highlight frenzy mode.
    
    .oktay name sends the greeting and without any parameters channel
    highlight mode is assumed.

--- plugins/plugin_oktay_lol.c
+++ plugins/plugin_oktay_lol.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
+#include <unistd.h>
 
 struct plugin_t * plugin;
 
@@ -19,27 +20,15 @@ static char * get_param(char * trailing)
     return param;
 }
 
-void run(void)
-{
 #define MAX_HOI_NUM 50 // 3 * X <=~ 500
-    char response[512], request[512], buffer[512];
-    long n = MAX_HOI_NUM;
-    char *level = NULL;
-
-    response[0] = request[0] = buffer[0] = 0;
-
-    level = get_param(plugin->irc->message.trailing);
-    if (level != NULL) { 
-        n = strtol(level, NULL, 10);
-        if (n <= 0 || n > MAX_HOI_NUM) n = MAX_HOI_NUM;
-    }
-
-
+static void lolz(char *buffer, size_t size, int level)
+{
+    int n = level;
     size_t written = 0;
     char hoi[] = "hohiohoih";
     char fmt[] = {'%', '.', '1', 's', '\0'};
     const size_t hoilen = strlen(hoi);
-    while (n-- && written < 450) {
+    while (n-- && written < size) {
         int i;
         int start = rand() % 5;
         int end = hoilen-1;
@@ -55,10 +44,79 @@ void run(void)
         snprintf(buffer + written, hl, fmt, hoi);;
         written += hl - 1;
     }
+}
 
-    sprintf(response, "PRIVMSG %s :%s", plugin->irc->from, buffer);
+static void highlight_channel(char *buffer, size_t size, struct channel_t *channel)
+{
+    char *buf = buffer;
+    char **nick = channel->users;
+    if (!channel->users)
+        return;
+    size_t written = 0;
 
-    plugin->send_message(plugin->irc, response);
+    while (written < size && *nick) {
+        size_t s = snprintf(buf, size - written, "%s ", *nick);
+        written += s;
+        nick++;
+        buf += written;
+    }
+}
+
+static void greet(char *buffer, size_t size, const char *name)
+{
+    const char *greetings[] = {
+        "sen ne guzel bir insansin %s",
+        "gotunu esek siksin %s",
+        "abin senin billigini yesin %s",
+        "ben geldim %s",
+    };
+    const size_t num_greetings = (sizeof greetings) / (sizeof greetings[0]);
+
+    int index = rand() % num_greetings;
+
+    snprintf(buffer, size, greetings[index], name);
+}
+
+void run(void)
+{
+    char response[512], request[512], buffer[512];
+    long n = MAX_HOI_NUM;
+    char *param = NULL;
+    struct channel_t *channel = NULL;
+
+    response[0] = request[0] = buffer[0] = 0;
+
+    param = get_param(plugin->irc->message.trailing);
+    if (param != NULL) {
+        n = strtol(param, NULL, 10);
+        if (n == 0 ) {
+            greet(buffer, 450, param);
+        } else {
+            if (n <= 0 || n > MAX_HOI_NUM) n = MAX_HOI_NUM;
+            lolz(buffer, 450, n);
+        }
+        sprintf(response, "PRIVMSG %s :%s", plugin->irc->from, buffer);
+
+        plugin->send_message(plugin->irc, response);
+    } else if (plugin->irc->from[0] == '#') {
+        struct channel_t **c = plugin->irc->channels_v;
+        while (*c) {
+            if (!strcmp((*c)->name, plugin->irc->from)) {
+                channel = *c;
+                break;
+            }
+        }
+        int count = 3 + (rand() % 5);
+        int i;
+        highlight_channel(buffer, 450, channel);
+        for (i = 0; i < count; ++i) {
+            sprintf(response, "PRIVMSG %s :%s", plugin->irc->from, buffer);
+
+            plugin->send_message(plugin->irc, response);
+            sleep(1);
+        }
+    }
+
 }
 
 struct plugin_t * init(void)
