7e11146e319dc4159d7d03d53e8e4da62030a2b9
Firat Salgur <firats.danisman@allianz.com.tr>
2015-11-24 14:50:21 +0200

    add dynamic python linker

---
+++ dpl.c
@@ -0,0 +1,69 @@
+#include "dpl.h"
+
+#include <stdio.h>
+
+#include <windows.h>
+
+typedef     void *                      PyObject;
+void        (*Py_Initialize)            (void);
+void        (*PyList_Append)            (PyObject *, PyObject *);
+PyObject    (*PySys_GetObject)          (char *);
+PyObject    (*PyString_FromString)      (char *);
+PyObject    (*PyImport_ImportModule)    (char *);
+PyObject    (*PyObject_GetAttrString)   (PyObject *, char *);
+int         (*PyTuple_SetItem)          (PyObject *, size_t pos, PyObject *);
+int         (*PyCallable_Check)         (PyObject *);
+PyObject    (*PyTuple_New)              (int);
+PyObject    (*PyObject_CallObject)      (PyObject *, PyObject *);
+char *      (*PyString_AsString)        (PyObject *);
+
+static HMODULE module;
+
+int get_symbol_address(char * sym_name)
+{
+    char msg_buf[1024];
+
+    snprintf(msg_buf, sizeof (msg_buf), "Getting address of symbol: %s", sym_name);
+    fprintf(stdout, "%s", msg_buf);
+
+    if (!(Py_Initialize = (void *) GetProcAddress(module, sym_name))) {
+        fprintf(stdout, " [FAIL]\n");
+        return -1;
+    }
+    fprintf(stdout, " [OK]\n");
+
+    return 0;
+}
+
+int load_dynamic_library(char * lib_name)
+{
+    char msg_buf[1024];
+
+    snprintf(msg_buf, sizeof (msg_buf), "Loading dynamic shared library: %s", lib_name);
+    fprintf(stdout, "%s", msg_buf);
+
+    if (!(module = LoadLibrary(lib_name))) {
+        fprintf(stdout, " [FAIL]\n");
+        return -1;
+    };
+    fprintf(stdout, " [OK]\n");
+
+    return 0;
+}
+
+int dpl_load_python(void)
+{
+    return
+        (load_dynamic_library("python27.dll")) ||
+        (get_symbol_address("Py_Initialize")) ||
+        (get_symbol_address("PySys_GetObject")) ||
+        (get_symbol_address("PyString_FromString")) ||
+        (get_symbol_address("PyImport_ImportModule")) ||
+        (get_symbol_address("PyObject_GetAttrString")) ||
+        (get_symbol_address("PyTuple_SetItem")) ||
+        (get_symbol_address("PyCallable_Check")) ||
+        (get_symbol_address("PyTuple_New")) ||
+        (get_symbol_address("PyObject_CallObject")) ||
+        (get_symbol_address("PyString_AsString"))
+    ;
+}
---
+++ dpl.h
@@ -0,0 +1,6 @@
+#ifndef __DPL_H_
+#define __DPL_H_
+
+int dpl_load_python(void);
+
+#endif // __DPL_H_
